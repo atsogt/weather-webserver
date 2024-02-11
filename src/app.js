51d7b9a60652c3a23f5013cb332ae720a3032baf
atsogt <atsogt17@gmail.com>
2024-02-10 21:19:13 -0500

    second commit

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const hbs = require("hbs");
 const forecast = require("../src/utils/forecast");
 const geocode = require("../src/utils/geocode");
-
+//test
 const app = express();
 
 const publicDirectory = path.join(__dirname, "../public");
